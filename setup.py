c60fa306668ff73eb4a0535ab8b22b2c64f908e6
NZ99 <o-pgk0l~i8-?Y|_IDGFh6da[_AtD>
2021-03-10 13:10:52 +0100

    Push to PyPI

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='transformer-in-transformer-flax',
     packages=find_packages(),
-    version='0.0.4',
+    version='0.0.1',
     license='MIT',
     description='Transformer in Transformer - Flax',
     author='Niccolò Zanichelli',
